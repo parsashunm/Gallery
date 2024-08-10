c456f5dd92ff35b62391ac2ae7170e3eea5fc3e9
parsashunm <parsagharib48@gmail.com>
2024-08-10 14:05:54 +0330

    5/20 - ready for MVP deploy

--- NFTgallery/NFTgallery/settings.py
+++ NFTgallery/NFTgallery/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
