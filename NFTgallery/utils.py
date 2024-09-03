a81f78756a79b36bac000d6324e8360e1de095f3
parsashunm <parsagharib48@gmail.com>
2024-09-03 19:33:24 +0330

    fix send OTP code bugs

--- NFTgallery/utils.py
+++ NFTgallery/utils.py
@@ -1,32 +1,36 @@
+import datetime
 import json
+import uuid
+import ghasedak_sms
 import requests
 
 
 def send_otp(num, code):
     # instance
-    # sms_api = ghasedak_sms.Ghasedak(api_key="be8c7924979fd0faa68edd5fa269b3795017b6ab131129e58e41b940450ad5e1xixsT2zP5aRFhkEx")
-    # res = sms_api.send_otp_sms(num, code)
-
+    sms_api = ghasedak_sms.Ghasedak(api_key="be8c7924979fd0faa68edd5fa269b3795017b6ab131129e58e41b940450ad5e1xixsT2zP5aRFhkEx")
+    cri = str(uuid.uuid4())
     # new one
     payload = json.dumps({
         "receptors": [
             {
                 "mobile": num,
-                "clientReferenceId": "8"
+                "clientReferenceId": cri
             }
         ],
-        'templateName': 'Ghasedak',
+        'templateName': 'Negar',
         "inputs": [
             {
                 'param': "code",
                 "value": code
             }
         ],
+        'udh': True
     })
     headers = {
         'Content-Type': 'application/json',
         'ApiKey': "be8c7924979fd0faa68edd5fa269b3795017b6ab131129e58e41b940450ad5e1xixsT2zP5aRFhkEx",
     }
 
-    requests.request('POST', url="https://gateway.ghasedak.me/rest/api/v1/WebService/SendOtpSMS",
-                     headers=headers, data=payload)
+    res = requests.request('POST', url="https://gateway.ghasedak.me/rest/api/v1/WebService/SendOtpSMS",
+                           headers=headers, data=payload)
+    print(f'{cri} {res.status_code}')
