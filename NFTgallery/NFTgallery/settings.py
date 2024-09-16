09eb252a780595d8092ba8e64f3703c912687a78
AlirezaM02 <alirezamirzaei68@gmail.com>
2024-09-16 14:16:58 +0300

    Server deploy Commit

--- NFTgallery/NFTgallery/settings.py
+++ NFTgallery/NFTgallery/settings.py
@@ -92,23 +92,22 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': '',
+         'USER': '',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'negar',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- NFTgallery/products/models.py
+++ NFTgallery/products/models.py
@@ -81,12 +81,10 @@ class Category(MP_Node):
 
     def __str__(self):
         return self.title
-
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
-        
-        
+
 class OptionGroup(models.Model):
     title = models.CharField(max_length=255, db_index=True)
 
