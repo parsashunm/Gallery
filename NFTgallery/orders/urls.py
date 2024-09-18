e93a695e524fad74416c32eaf2c3721a6e952d60
parsashunm <parsagharib48@gmail.com>
2024-09-18 11:28:46 +0330

    change buy product to orders / bug fix / add api to urls

--- NFTgallery/orders/urls.py
+++ NFTgallery/orders/urls.py
@@ -6,11 +6,11 @@ from .views import BuyProductView, VerifyPurchaseView, ChargeWalletView
 app_name = 'orders'
 
 urlpatterns = [
-    path('products/buy/<int:product_id>/', BuyProductView.as_view(), name='buy_product'),
+    path('product/buy/<int:product_id>/', BuyProductView.as_view(), name='buy_product'),
     # path('purchase/<int:user_id>/<int:price>/', ChargeWalletView.as_view(), name='purchase_product'),
     # path('purchase/verify/', VerifyPurchaseView.as_view(), name='verify_purchase'),
     # az_bank_gateways
     # path("bankgateways/", az_bank_gateways_urls(), name='purchase_product'),
-    # path("dol/", az_bank_gateways_urls(), name='verify_purchase'),
+    # path("test/", az_bank_gateways_urls(), name='verify_purchase'),
 
 ]
