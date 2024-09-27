35387a1c0c24b48cbe2e9d83746a0c051d21cb48
parsashunm <parsagharib48@gmail.com>
2024-09-27 09:13:31 +0330

    permissions updated

--- NFTgallery/products/models.py
+++ NFTgallery/products/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
     is_sold = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
 
