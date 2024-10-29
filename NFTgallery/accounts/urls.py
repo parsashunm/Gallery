0662f91ba8120dec54980d1e381f1781b6075bc8
parCa <parsagharib48@gmail.com>
2024-10-29 21:32:34 +0330

    user profile added / urls have been changed

--- NFTgallery/accounts/urls.py
+++ NFTgallery/accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('user/register/confirm/', ConfirmOtpView.as_view(), name='confirm_code'),
     path('user/login/', UserLoginView.as_view(), name='user_login'),
     path('user/logout/', UserLogOutView.as_view(), name='user_logout'),
-    path('user/profile/create/', CreateUserProfileView.as_view(), name="profile_create"),
-    path('user/profile/edit/<int:user>/', UserEditProfileView.as_view(), name="profile_edit"),
+    # path('user/profile/create/', CreateUserProfileView.as_view(), name="profile_create"),
+    # path('user/profile/edit/<int:user>/', UserEditProfileView.as_view(), name="profile_edit"),
 ]
